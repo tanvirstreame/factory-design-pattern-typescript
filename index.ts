2a9de94e724ed9f43c9fd15e42399bcc027467ed
Tanvir Islam Streame <engr.tanvir.islam.1@gmail.com>
2022-12-25 03:39:42 +0600

    Create index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world");
+import PizzaFactory from "./factory/pizza.factory";
+import PizzaStore from "./store/pizza.store";
+
+let pizzaFactory = new PizzaFactory();
+let pizzaStore = new PizzaStore(pizzaFactory);
+console.log("order cheese pizza",pizzaStore.orderPizza("cheese"));
+console.log("order pepperoni pizza",pizzaStore.orderPizza("pepperoni"));
